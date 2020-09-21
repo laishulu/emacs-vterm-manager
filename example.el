d770fd8cff7c24688199392ad93c01485c6a9569
Mao <code@mao.to>
2020-09-21 11:38:31 +0800

    update example vtm file

--- example.el
+++ example.el
@@ -6,25 +6,25 @@
  :commands
  (
 
-  ;; First command.
-  (
-   ;; Sleep before command, default to 0.
-   :sleep 0
-   ;; String to send, default to "".
-   :string "{CURSOR}"
-   ;; Additional control character to send.
-   ;; Possible values:
-   ;; - "key" in command ~vterm-send-{key}~: send ~key~
-   ;; - "": send ~return~.
-   ;; - nil: do not send control character.
-   ;; Default to "" (send ~return~).
-   :control ""
-   )
+  ;; ;; First command.
+  ;; (
+   ;; ;; Sleep before command, default to 0.
+   ;; :sleep 0
+   ;; ;; String to send, default to "".
+   ;; :string ""
+   ;; ;; Additional control character to send.
+   ;; ;; Possible values:
+   ;; ;; - "key" in command ~vterm-send-{key}~: send ~key~
+   ;; ;; - "": send ~return~.
+   ;; ;; - nil: do not send control character.
+   ;; ;; Default to "" (send ~return~).
+   ;; :control ""
+   ;; )
 
   ;; ;; Second command.
   ;; (:sleep 0 :string "" :control "")
 
-  ;; ;; Third command.
-  ;; (:string "")
+  ;; Third command.
+  (:string "{CURSOR}")
 
   ))
